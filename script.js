d5f493485d8a4b462b398a59660877f98beca686
Kanruethai Jomsaeng <kanruethaijomsaeng@Kanruethais-Air.home>
2020-08-01 09:21:59 -0400

    update2

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ const link = document.getElementById("nav-link");
 
 navBtn.addEventListener("click", () =>{
     link.classList.toggle("show-link");
+
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+    e.preventDefault();
+    link.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    let position = element.offsetTop - 170;
+
+    window.scrollTo({
+        left: 0,
+        top: position,
+        behavior: "smooth"
+    });
+});
 });
